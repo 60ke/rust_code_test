1cd0743a4a737cc8900c7391271f217e2011ba56
k <worileqing@163.com>
2021-01-08 14:57:20 +0800

    添加reprint测试

--- macro_test/src/main.rs
+++ macro_test/src/main.rs
@@ -1,3 +1,4 @@
+use cfg_if::cfg_if;
 use macro_test_derive::macro_test_derive;
 
 #[macro_test_derive(K)]
@@ -5,6 +6,19 @@ fn hello(){
     unimplemented!()
 }
 
+macro_rules! reprint {
+    ($e:expr,$d:expr) => {
+        cfg_if!{
+            if #[cfg(feature = "test")]{
+                println!($e,$d)
+            }
+        }
+    };
+}
+
+
 fn main(){
-    hello1()
+    hello1();
+    reprint!("{}",3);
+
 }
